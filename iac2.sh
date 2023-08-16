35b4df4ed7c8f4ea1e6a5e349bfe03ac285fc0a8
Eduardo Santos <87313525+TheEduSantos@users.noreply.github.com>
2023-08-15 22:23:50 -0300

    Update iac2.sh

--- iac2.sh
+++ iac2.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-echo "=================================== Atualização e Instalação ==================================="
+echo "=================================== Update and Installation | Atualização e Instalação ==================================="
 sudo apt-get update && sudo apt-get dist-upgrade -y && sudo apt-get install apache2 -y && sudo apt-get install unzip -y && sudo apt autoremove -y
 
-echo "========================= Baixando e copiando os arquivos da aplicação ========================="
+echo "========================= Downloading and copying application files | Baixando e copiando os arquivos da aplicação ========================="
 
 cd /tmp || exit
 wget https://github.com/denilsonbonatti/linux-site-dio/archive/refs/heads/main.zip
@@ -11,4 +11,4 @@ unzip main.zip
 cd linux-site-dio-main || exit
 sudo cp -R * /var/www/html/
 
-echo "====================================== Processo concluído ======================================"
+echo "====================================== Process Completed | Processo Concluído ======================================"
